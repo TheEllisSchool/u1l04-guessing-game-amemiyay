ac9708ab6fca97f1a91484b762568225da456d05
yumikaamemiya <yumikaamemiya@172.16.21.132>
2017-09-26 10:51:25 -0400

    Random number

---
+++ src/GuessingGame.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		int computerNum = (int) (Math.random() * 100) + 1;
+		//JOptionPane.showMessageDialog(null, computerNum);
+		
+		
+
+	}
+
+}
